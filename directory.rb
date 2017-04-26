b8080783c687d59fe586a46dd9311600bb35168c
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-04-26 18:43:53 +0100

    8.7 adding cohort input

--- directory.rb
+++ directory.rb
@@ -7,16 +7,25 @@ def input_students
   name = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
-    puts "What country are you from?"
-    country = gets.chomp.upcase
-    puts "Favourite meal?"
-    meal = gets.chomp.downcase
-    # add the student hash to the array
-    students << {name: name, cohort: :november, country: country, food: meal}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    puts "New name?"
-    name = gets.chomp.capitalize
+    months = ["January", "February", "March",
+              "April", "May", "June",
+              "July", "August", "September",
+              "October", "November", "December"
+    ]
+    puts "What cohort?"
+    cohort = gets.chomp.capitalize
+      if months.include? (cohort)
+        puts "Favourite meal?"
+        meal = gets.chomp.downcase
+        # add the student hash to the array
+        students << {name: name, cohort: cohort, food: meal}
+        puts "Now we have #{students.count} students"
+        #get another name from the user
+        puts "New name?"
+        name = gets.chomp.capitalize
+      else
+        puts "Cohort not found, please re_enter."
+      end
   end
   # return the array of students
   students
@@ -24,8 +33,8 @@ end
 
 # and print them
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(75)
+  puts "-------------".center(75)
 end
 
 
@@ -35,7 +44,7 @@ def print(students)
   until count > students.count
     students.each_with_index.collect do |student, index|
       name = student[:name]
-      puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort) - from #{student[:country]} and loves to eat #{student[:food]}}"
+      puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort) - loves to eat #{student[:food]}".center(75)
       count += 1
   end
 end
@@ -43,7 +52,8 @@ end
 
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "-------------".center(75)
+  puts "Overall, we have #{names.count} great students".center(75)
 end
 # nothing happens until we call the methods
 students = input_students
